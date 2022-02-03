674118b7358d5eca1829934a56403d1e227cf062
MagicManik <manikislam1952@gmail.com>
2022-02-04 01:08:46 +0600

    reverse string

---
+++ reverse-string.js
@@ -0,0 +1,36 @@
+/* const message1 = 'Hello, How are you?';
+function reverseString(text) {
+    var store = '';
+    for (var element of text) {
+        store = element + store;
+    }
+    return store;
+}
+var result = reverseString(message1);
+console.log(result); */
+
+
+// practice reverse string
+/* function reverseString(text) {
+    var store = '';
+    for (var element of text) {
+        store = element + store;
+    }
+    return store;
+}
+var message2 = 'I love you!';
+var result = reverseString(message2);
+console.log(result); */
+
+
+// practice reverse string one more
+function reverseString(text) {
+    var store = '';
+    for (var element of text) {
+        store = element + store;
+    }
+    return store;
+}
+var message3 = 'I miss you!'
+var result = reverseString(message3);
+console.log(result);
